b2d5e77fe5c271de72299dc82a3ba3fed62b0d9b
UjiDavid <100906460+UjiDavid@users.noreply.github.com>
2023-09-28 14:55:26 +0100

    Initial commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import CustomerReviews from './CustomerReviews';
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import SuperQuality from './SuperQuality';
+import Subscribe from './Subscribe';
+import Footer from './Footer';
+import SpecialOffer from './SpecialOffer';
+import Services from './Services';
+
+export {
+  Hero,
+  PopularProducts,
+  SuperQuality,
+  Services,
+  Subscribe,
+  SpecialOffer,
+  CustomerReviews,
+  Footer,
+};
